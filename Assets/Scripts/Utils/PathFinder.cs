8b477c0176164e64e7d15cacf6dd304c752f9efa
Jamy4000 <arnaudbriche1994@gmail.com>
2020-04-23 18:27:50 +0200

    Finished algorithm, need test

--- Assets/Scripts/Utils/PathFinder.cs
+++ Assets/Scripts/Utils/PathFinder.cs
@@ -6,70 +6,68 @@ public static class PathFinder
 
     public static bool PathValidExist(Tile thisTile, Tile currentlyClickedTile)
     {
-        return true;
-
         foreach (var path in _availablePaths)
         {
-            if (path.From == thisTile && path.To == currentlyClickedTile)
+            if (path.From == currentlyClickedTile && path.To.Contains(thisTile))
                 return true;
         }
 
         return false;
     }
 
-    internal static void UpdateAvailablePaths()
+    public static void UpdateAvailablePaths()
     {
         var tileMatrix = GameManager.Instance.TilesMatrix;
         for (int i = 1; i < tileMatrix.Length - 1; i++)
         {
             for (int j = 1; j < tileMatrix[i].Length - 1; j++)
             {
+                List<Tile> neighbors = new List<Tile>();
                 if (tileMatrix[i][j] != null)
                 {
-                    //Tile neighbor = CheckNeighbors(i - 1, j, i, j, tileMatrix[i][j].ID, 0);
-                    //if (neighbor != null)
-                    //    _availablePaths.Add(new Path(tileMatrix[i][j], neighbor));
+                    CheckNeighbors(i - 1, j, i, j, 0, ref neighbors);
+                    if (neighbors.Count > 0)
+                        _availablePaths.Add(new Path(tileMatrix[i][j], neighbors));
 
-                    //neighbor = CheckNeighbors(i, j - 1, i, j, tileMatrix[i][j].ID, 0);
-                    //if (neighbor != null)
-                    //    _availablePaths.Add(new Path(tileMatrix[i][j], neighbor));
+                    neighbors.Clear();
+                    CheckNeighbors(i, j - 1, i, j, 0, ref neighbors);
+                    if (neighbors.Count > 0)
+                        _availablePaths.Add(new Path(tileMatrix[i][j], neighbors));
 
-                    //neighbor = CheckNeighbors(i + 1, j, i, j, tileMatrix[i][j].ID, 0);
-                    //if (neighbor != null)
-                    //    _availablePaths.Add(new Path(tileMatrix[i][j], neighbor));
+                    neighbors.Clear();
+                    CheckNeighbors(i + 1, j, i, j, 0, ref neighbors);
+                    if (neighbors.Count > 0)
+                        _availablePaths.Add(new Path(tileMatrix[i][j], neighbors));
 
-                    //neighbor = CheckNeighbors(i, j + 1, i, j, tileMatrix[i][j].ID, 0);
-                    //if (neighbor != null)
-                    //    _availablePaths.Add(new Path(tileMatrix[i][j], neighbor));
+                    neighbors.Clear();
+                    CheckNeighbors(i, j + 1, i, j, 0, ref neighbors);
+                    if (neighbors.Count > 0)
+                        _availablePaths.Add(new Path(tileMatrix[i][j], neighbors));
                 }
             }
         }
     }
 
-    private static bool CheckNeighbors(int i, int j, int originalI, int originalJ, string tileIndex, int cornerSinceBeginning, ref List<Tile> toReturn)
+    private static void CheckNeighbors(int i, int j, int originalI, int originalJ, int cornerSinceBeginning, ref List<Tile> toReturn)
     {
-        return false;
-
         var tileMatrix = GameManager.Instance.TilesMatrix;
 
         // if the current neighbor isn't an empty tile
-        if (!tileMatrix[i][j].IsEmpty) 
+        if (tileMatrix[i][j] != null && !tileMatrix[i][j].IsEmpty) 
         {
-            if (tileMatrix[i][j].ID == tileMatrix[originalI][originalJ].ID) 
+            if (tileMatrix[i][j].ID == tileMatrix[originalI][originalJ].ID && i != originalI && j != originalJ) 
             {
                 toReturn.Add(tileMatrix[i][j]);
-                return true;
-            }
-            else 
-            {
-                return false;
             }
         }
         else 
         {
             for (int x = -1; x < 2; x += 2)
             {
-
+                for (int y = -1; y < 2; y += 2)
+                {
+                    CheckNeighbors(i + x, j + y, originalI, originalJ, cornerSinceBeginning + 1, ref toReturn);
+                }
             }
         }
     }
@@ -78,9 +76,9 @@ public static class PathFinder
 public struct Path 
 {
     public Tile From;
-    public Tile To;
+    public List<Tile> To;
 
-    public Path(Tile from, Tile to) 
+    public Path(Tile from, List<Tile> to) 
     {
         From = from;
         To = to;
